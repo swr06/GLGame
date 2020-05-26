6a037b54a43a6dec6b58049cfd03b98a10fff85d
Samuel Rasquinha <samuelrasquinha@gmail.com>
2020-05-26 17:35:32 +0530

    Shader optimizations.
    Shader Optimizations.
    Multiplying the ambient color and texColor in the vertex shader instead instead of multiplying it in the fragment shader.

--- GLGame/Core/Shaders/DefaultBatchFragShader.glsl
+++ GLGame/Core/Shaders/DefaultBatchFragShader.glsl
@@ -1,13 +1,12 @@
 #version 330 core
 in vec2 TexCoord;
 in flat int TexElement;
-in flat vec4 TexColor;
+in flat vec4 MultiplyColor;
 out vec4 color;
 
-uniform vec4 u_AmbientColor;
 uniform sampler2D u_Textures[32];
 
 void main()
 {
-	color = u_AmbientColor * TexColor * texture(u_Textures[TexElement], TexCoord);
+	color = MultiplyColor * texture(u_Textures[TexElement], TexCoord);
 }
--- GLGame/Core/Shaders/DefaultBatchVertexShader.glsl
+++ GLGame/Core/Shaders/DefaultBatchVertexShader.glsl
@@ -6,14 +6,15 @@ layout(location = 3) in float texElement;
 
 out vec2 TexCoord;
 out int TexElement;
-out vec4 TexColor;
+out vec4 MultiplyColor;
 
+uniform vec4 u_AmbientColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 uniform mat4 u_ViewProjectionMatrix = mat4(1.0f);
 
 void main()
 {
 	gl_Position = u_ViewProjectionMatrix * vec4(position, 1.0f); 
-	TexColor = texColor;
+	MultiplyColor = u_AmbientColor * texColor;
 	TexCoord = texCoord; 
 	TexElement = int(texElement);
 }
